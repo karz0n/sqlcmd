baaf76726d11869ad2de26f17b606bee671db1af
karz0n <d.asauliak@gmail.com>
2017-12-02 20:29:43 +0200

    refactor find test

--- src/test/java/ua/in/denoming/sqlcmd/controller/FindTest.java
+++ src/test/java/ua/in/denoming/sqlcmd/controller/FindTest.java
@@ -2,11 +2,8 @@ package ua.in.denoming.sqlcmd.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 import org.mockito.ArgumentCaptor;
-import static org.mockito.Mockito.*;
-
 import ua.in.denoming.sqlcmd.model.DataSet;
 import ua.in.denoming.sqlcmd.model.DataSet.Data;
 import ua.in.denoming.sqlcmd.model.DatabaseManager;
@@ -17,33 +14,34 @@ import ua.in.denoming.sqlcmd.view.View;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 class FindTest {
     private View view;
     private DatabaseManager manager;
+    private Command command;
 
     @BeforeEach
     void setup() {
         view = mock(View.class);
         manager = mock(DatabaseManager.class);
+        command = new Find(view, manager);
     }
 
     @Test
     void testCanExecute() {
-        Command command = new Find(view, manager);
         assertTrue(command.canExecute("test"));
         assertFalse(command.canExecute());
     }
 
     @Test
     void testWrongCallOfExecute() {
-        Command command = new Find(view, manager);
         assertThrows(WrongCountOfArgumentsException.class, command::execute);
     }
 
     @Test
     void testExecute() {
-        Command command = new Find(view, manager);
-
         ArrayList<DataSet> sets = new ArrayList<>();
         sets.add(new DataSet(
             new Data("name1", "value1"),
@@ -53,12 +51,16 @@ class FindTest {
 
         command.execute("test");
 
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(view, atLeastOnce()).writeLine(captor.capture());
-        assertEquals("[+------+------+\n" +
+        assertPrint("[+------+------+\n" +
             "|name1 |name2 |\n" +
             "+------+------+\n" +
             "|value1|value2|\n" +
-            "+------+------+]", captor.getAllValues().toString());
+            "+------+------+]");
+    }
+
+    private void assertPrint(String expected) {
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(view, atLeastOnce()).writeLine(captor.capture());
+        assertEquals(expected, captor.getAllValues().toString());
     }
 }
