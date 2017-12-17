0af03cb869a446d25b837fe0d63da4c604642a02
karz0n <d.asauliak@gmail.com>
2017-12-17 22:07:51 +0200

    remove junk code

--- src/main/java/ua/in/denoming/sqlcmd/Main.java
+++ src/main/java/ua/in/denoming/sqlcmd/Main.java
@@ -2,16 +2,8 @@ package ua.in.denoming.sqlcmd;
 
 import ua.in.denoming.sqlcmd.controller.App;
 
-class Pair<T> {
-    private T first;
-    private T second;
-}
-
 public class Main {
     public static void main(String[] args) {
-        @SuppressWarnings("unchecked")
-        Pair<String>[] table = (Pair<String>[]) new Pair<?>[10];
-
         try (App app = new App()) {
             app.run();
         } catch (Exception e) {
