0c5a116827064bce4d0b4e30e79bc7286633713c
karz0n <d.asauliak@gmail.com>
2017-12-19 22:08:30 +0200

    append throwing exception

--- src/main/java/ua/in/denoming/sqlcmd/model/command/Clear.java
+++ src/main/java/ua/in/denoming/sqlcmd/model/command/Clear.java
@@ -1,6 +1,7 @@
 package ua.in.denoming.sqlcmd.model.command;
 
 import ua.in.denoming.sqlcmd.model.DatabaseManager;
+import ua.in.denoming.sqlcmd.model.exception.WrongCommandArgumentsException;
 import ua.in.denoming.sqlcmd.view.View;
 
 public class Clear implements Command {
@@ -21,9 +22,12 @@ public class Clear implements Command {
 
     @Override
     public void execute(String... args) {
+        if (!canExecute(args)) {
+            throw new WrongCommandArgumentsException();
+        }
+
         String tableName = args[0];
         databaseManager.clearTable(tableName);
-
         view.writeFormatLine("Table '%s' has cleared successfully", tableName);
     }
 }
