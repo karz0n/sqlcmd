da4b7c35104605742a13cfe6e36b1ca9ea98b0f4
karz0n <d.asauliak@gmail.com>
2017-12-19 20:50:32 +0200

    refactor find test

--- src/test/java/ua/in/denoming/sqlcmd/controller/FindTest.java
+++ src/test/java/ua/in/denoming/sqlcmd/controller/FindTest.java
@@ -16,6 +16,7 @@ import ua.in.denoming.sqlcmd.view.View;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.*;
 
 class FindTest {
@@ -34,25 +35,39 @@ class FindTest {
     void testCanExecute() {
         assertTrue(command.canExecute("test"));
         assertFalse(command.canExecute());
+        assertFalse(
+            command.canExecute("too", "many", "arguments")
+        );
     }
 
     @Test
     void testWrongCallOfExecute() {
         assertThrows(WrongCommandArgumentsException.class, command::execute);
+        assertThrows(
+            WrongCommandArgumentsException.class, () -> command.execute("too", "many", "arguments")
+        );
     }
 
     @Test
     void testExecute() {
+        //
+        // Given
+        //
         ArrayList<DataSet> sets = new ArrayList<>();
         sets.add(new DataSet(
             new Data("name1", "value1"),
             new Data("name2", "value2")
         ));
-
         when(manager.obtainTableData("test")).thenReturn(sets);
 
+        //
+        // When
+        //
         command.execute("test");
 
+        //
+        // Then
+        //
         String expected =
             "+----------+----------+" + System.lineSeparator() +
             "|  name1   |  name2   |" + System.lineSeparator() +
@@ -65,9 +80,6 @@ class FindTest {
     private void assertPrint(String expected) {
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
         verify(view, atLeastOnce()).writeLine(captor.capture());
-        assertEquals(
-            expected,
-            captor.getAllValues().iterator().next()
-        );
+        assertEquals(expected, captor.getValue());
     }
 }
